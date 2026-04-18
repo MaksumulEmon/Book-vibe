d35d3b410cfe391ff7161e7eb37f3b8c36b92b8b
MaksumulEmon <maksumulemon300@gmail.com>
2026-04-18 20:02:58 +0600

    store  read

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
 
-    const { bookId : bookParamsId} = useParams();
-    console.log(bookParamsId, "bookId");
+    const { bookId: bookParamsId } = useParams();
+    // console.log(bookParamsId, "bookId");
 
     const books = useLoaderData();
-    console.log(books)
+    // console.log(books)
 
-    const expectedBook = books.find((book) => book.bookId === Number( bookParamsId));
-    console.log(expectedBook)
+    const expectedBook = books.find((book) => book.bookId === Number(bookParamsId));
+    // console.log(expectedBook)
 
-    const { bookId, category, rating, totalPages, image, author, bookName, review, tags } = expectedBook;
+    const { bookId, image, author, bookName, review, tags } = expectedBook;
+
+
+    const [storeBooks, setStoreBooks] = useState([]);
+
+
+    const handleMarkread = (currentBook) => {
+
+        console.log(currentBook);
+        const isexistBook = storeBooks.find((book) => book.bookId ===  currentBook.bookId);
+
+        if(isexistBook){
+            alert("The is all reddy")
+        }else{
+            setStoreBooks([...storeBooks, currentBook])
+        }
+    }
+
+
     return (
-        
+
         <div className="grid grid-cols-1 lg:grid-cols-2 bg-base-100 shadow-sm max-w-7xl mx-auto p-5">
             <figure className=' flex items-center w-full justify-center bg-gray-100'>
                 <img
                     src={image}
-                    alt="Album"  className='h-100'/>
+                    alt="Album" className='h-100' />
             </figure>
 
             <div className="card-body">
@@ -27,15 +45,15 @@ const BookDetails = () => {
                 <p>{author}</p>
                 <p>{review}</p>
 
-               <div className='flex gap-4'>
-                 {
-                    tags.map((tag,ind) => {
-                        return <p key={ind} className='bg-green-500 text-green-100 py-2 px-3 rounded-2xl'> {tag}</p>
-                    })
-                }
-               </div>
+                <div className='flex gap-4'>
+                    {
+                        tags.map((tag, ind) => {
+                            return <p key={ind} className='bg-green-500 text-green-100 py-2 px-3 rounded-2xl'> {tag}</p>
+                        })
+                    }
+                </div>
                 <div className="card-actions">
-                    <button className="btn btn-primary">Read</button>
+                    <button onClick={() => handleMarkread(expectedBook)} className="btn btn-primary">Read</button>
                     <button className="btn btn-primary">WishList</button>
                 </div>
             </div>
